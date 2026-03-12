bb7700138bb14a31507d61785ac0cf7862f88ccf
DraganovTs <cvetomir_draganov@abv.bg>
2026-03-12 20:30:38 +0200

    Set up Spring Boot application with OAuth2 integration using Keycloak

---
+++ src/main/java/org/c2/keycloak/oauth2/training/KeycloakOauth2Application.java
@@ -0,0 +1,12 @@
+package org.c2.keycloak.oauth2.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakOauth2Application.class, args);
+    }
+}
---
+++ src/main/java/org/c2/keycloak/oauth2/training/controller/KeycloakController.java
@@ -0,0 +1,9 @@
+package org.c2.keycloak.oauth2.training.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class KeycloakController {
+}

af66c31b8c69f85735177aecf99c573fd1d8406f
DraganovTs <cvetomir_draganov@abv.bg>
2026-03-13 19:54:16 +0200

    Add HomeController and Thymeleaf-based home page with OAuth2 user details display

--- src/main/java/org/c2/keycloak/oauth2/training/config/SecurityConfig.java
+++ src/main/java/org/c2/keycloak/oauth2/training/config/SecurityConfig.java
@@ -4,22 +4,40 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        OidcClientInitiatedLogoutSuccessHandler handler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+
+        handler.setPostLogoutRedirectUri("{baseUrl}");
+
         return http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers("/logout"))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "error").permitAll()
+                        .requestMatchers("/login", "/error" ).permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2
                         .loginPage("/login")
-                        .defaultSuccessUrl("/home"))
+                        .defaultSuccessUrl("/home" ,true))
+                .logout(logout ->
+                        logout.logoutSuccessHandler(handler))
                 .build();
 
     }

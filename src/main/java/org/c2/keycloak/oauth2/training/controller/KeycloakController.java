8de64e8926ba964b5c2055e3a7a9f7fedc2c427e
DraganovTs <cvetomir_draganov@abv.bg>
2026-03-13 19:13:59 +0200

    Replace KeycloakController with LoginController and add Thymeleaf-based login page

--- src/main/java/org/c2/keycloak/oauth2/training/controller/KeycloakController.java
+++
@@ -1,9 +0,0 @@
-package org.c2.keycloak.oauth2.training.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public class KeycloakController {
-}
---
+++ src/main/java/org/c2/keycloak/oauth2/training/controller/LoginController.java
@@ -0,0 +1,13 @@
+package org.c2.keycloak.oauth2.training.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
